a8e30e885fa839b0de4373e5270f9a4808f38ab9
Faisal Ali-Adebambo <oluwabori2004@gmail.com>
2023-11-01 01:18:15 -0700

    hello

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -13,27 +13,27 @@ char *new_string = NULL;
 int k = 0, i = ac, j, sum = 0, temp = 0;
 
 if (ac == 0 || av == NULL)
-	return (NULL);
+return (NULL);
 
 while (ac--)
-	sum += (len(av[ac]) + 1);
+sum += (len(av[ac]) + 1);
 new_string = (char *) malloc(sum + 1);
 
 if (new_string != NULL)
 {
-	while (k < i)
-	{
+while (k < i)
+{
 for (j = 0; av[k][j] != '\0'; j++)
-	new_string[j + temp] = av[k][j];
+new_string[j + temp] = av[k][j];
 new_string[temp + j] = '\n';
 temp += (j + 1);
 k++;
-	}
-	new_string[temp] = '\0';
+}
+new_string[temp] = '\0';
 }
 else
 {
-	return (NULL);
+return (NULL);
 }
 return (new_string);
 }
@@ -49,9 +49,9 @@ int len = 0;
 
 if (str != NULL)
 {
-	while (str[len])
+while (str[len])
 len++;
 }
-	return (len);
+return (len);
 }
 
